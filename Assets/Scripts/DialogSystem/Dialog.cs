6e13d0670b233b4f676eaaf06d14786eebd620a1
Artem Ruts <justartem@gmail.com>
2015-04-27 21:59:23 +0300

    Creating Dialog constructor, DialogDatabse.

---
+++ Assets/Scripts/DialogSystem/Dialog.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Dialog : MonoBehaviour{
+
+	public string name;
+	public int count;
+	public int index;
+	public Button answer;
+	
+	public Dialog(string fileName, int answCount, int ID)
+	{
+		name = fileName;
+		count = answCount;
+		index = ID;
+	}
+
+
+}
---
+++ Assets/Scripts/DialogSystem/DilogDatabase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DilogDatabase : MonoBehaviour {
+
+	public List<Dialog> dilog = new List<Dialog>();
+
+	void Start()
+	{
+		dilog.Add(new Dialog("testText01", 2, 0));
+		dilog.Add(new Dialog("testText02", 1, 1));
+		dilog.Add(new Dialog("testText03", 3, 2));
+	}
+}
