19ffb29f22f4d77c821b6d384a872f0f3a9f1108
Maksim Ruts <tribar.ge@gmail.com>
2015-06-05 19:03:36 +0300

    Clearing trash

--- Assets/Scripts/ScreenFader.cs
+++ Assets/Scripts/ScreenFader.cs
@@ -2,57 +2,48 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class ScreenFader : MonoBehaviour {
-
+public class ScreenFader : MonoBehaviour
+{
+    public Image FadeImg;
     public float fadeSpeed = 1.5f;
     public bool sceneStarting = true;
-    RawImage FadeImg;
 
-    void Awake(){
-        BlackTexture(Color.black);
-    }
 
-    void BlackTexture(Color clr)
+    void Awake()
     {
-        GameObject canvas = GameObject.FindGameObjectWithTag("Canvas");
-        GameObject ImgObject = new GameObject();
-        ImgObject.transform.parent = canvas.transform;
-        ImgObject.AddComponent<RawImage>();
-        FadeImg = ImgObject.GetComponent<RawImage>();
-        FadeImg.material.color = clr;
-        FadeImg.SetNativeSize();
+        FadeImg.rectTransform.localScale = new Vector2(Screen.width, Screen.height);
     }
 
-    void Update ()
+    void Update()
     {
         // If the scene is starting...
-        if(sceneStarting)
+        if (sceneStarting)
             // ... call the StartScene function.
             StartScene();
     }
 
 
-    void FadeToClear ()
+    void FadeToClear()
     {
         // Lerp the colour of the image between itself and transparent.
         FadeImg.color = Color.Lerp(FadeImg.color, Color.clear, fadeSpeed * Time.deltaTime);
     }
 
 
-    void FadeToBlack ()
+    void FadeToBlack()
     {
         // Lerp the colour of the image between itself and black.
         FadeImg.color = Color.Lerp(FadeImg.color, Color.black, fadeSpeed * Time.deltaTime);
     }
 
 
-    void StartScene ()
+    void StartScene()
     {
         // Fade the texture to clear.
         FadeToClear();
 
         // If the texture is almost clear...
-        if(FadeImg.color.a <= 0.05f)
+        if (FadeImg.color.a <= 0.05f)
         {
             // ... set the colour to clear and disable the RawImage.
             FadeImg.color = Color.clear;
@@ -64,7 +55,7 @@ public class ScreenFader : MonoBehaviour {
     }
 
 
-    public void EndScene (int SceneNumber)
+    public void EndScene(int SceneNumber)
     {
         // Make sure the RawImage is enabled.
         FadeImg.enabled = true;
@@ -73,9 +64,8 @@ public class ScreenFader : MonoBehaviour {
         FadeToBlack();
 
         // If the screen is almost black...
-        if(FadeImg.color.a >= 0.95f)
-            // ... reload the level.
+        if (FadeImg.color.a >= 0.95f)
+            // ... reload the level
             Application.LoadLevel(SceneNumber);
     }
-}
-
+}   
