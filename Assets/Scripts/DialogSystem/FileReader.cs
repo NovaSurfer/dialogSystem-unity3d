1fb9992058d02f64e922b3a0ea3ba455f7ba1ad3
Maksim Ruts <tribar.ge@gmail.com>
2015-06-05 20:59:19 +0300

    Adding comments (RUS)

--- Assets/Scripts/DialogSystem/FileReader.cs
+++ Assets/Scripts/DialogSystem/FileReader.cs
@@ -9,38 +9,38 @@ using System;
 
 public class FileReader : MonoBehaviour{
 
-	public string defaultDilogFile;
-	public Text dialogWindowText;
-	public Button answerButton;
-	int y = -130; 
+	public string defaultDilogFile;         // Начальный файл с диалогом
+	public Text dialogWindowText;           // Элемент *текста* главного окна диалога
+	public Button answerButton;             // Префаб с кнопкой ответа
+	int y = -110;                           // Начальная координата первой кнопки с ответом по оси Y
 
 	public void ReadFromFile(string fileName)
     {
 		string[] answersArray;
-		TextAsset mydata = Resources.Load(fileName, typeof(TextAsset)) as TextAsset; 
-		string[] fileLines = mydata.text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-		dialogWindowText.text = fileLines[0];
-		for(int i = 1; i < fileLines.Length; i++) {
-			if(answerButton != null) {
-				Button ansBtn = (Button)Instantiate(answerButton);
-				answersArray = fileLines[i].Split(new string[] { "#->" }, StringSplitOptions.None);
+        TextAsset mydata = Resources.Load(fileName, typeof(TextAsset)) as TextAsset;                                            // Текстовый файл загружаемый из папки Resources
+		string[] fileLines = mydata.text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);    // Разбиваем текстовый файл на строки и добавляем в массив, при этом игнорируем пустые строки.
+		dialogWindowText.text = fileLines[0];                                                                                   // Присваиваем первую строку тексту главного диалогового окна
+		for(int i = 1; i < fileLines.Length; i++) {                                                                             // Игнорируя первую строку запускаем цикл для создания ответов
+			if(answerButton != null) {                                                                                          
+				Button ansBtn = (Button)Instantiate(answerButton);                                                              // Создание кнопки на сцене
+				answersArray = fileLines[i].Split(new string[] { "#->" }, StringSplitOptions.None);                             // Режем строку с текстом ответа до тега "#->"
 				//Debug.Log("["+answersArray[0]+"]" + "["+answersArray[1]+"]");
-				ansBtn.GetComponent<nextDilog>().file = answersArray[1];
-				ansBtn.transform.parent = this.gameObject.transform;
-				ansBtn.GetComponent<RectTransform>().localPosition = new Vector3(0, y = y - 43, 0);
-				Text answText = ansBtn.GetComponentInChildren<Text>();
-				answText.text = answersArray[0];
+                ansBtn.GetComponent<nextDilog>().file = answersArray[1];                                                        // Второй "кусок", отрезанной выше строки answersArray[1], присваиваем переменной file класса nextDilog
+				ansBtn.transform.parent = this.gameObject.transform;                                                            // Делаем созданную кнопку дочерней по отношению к Canvas (!для этого скрипт должен быть прикреплен к Canvas!)
+				ansBtn.GetComponent<RectTransform>().localPosition = new Vector3(0, y = y - 43, 0);                             // Смещаем нашу кнопку вниз на 43 пикселя от позиции последней кнопки (только по оси Y)
+				Text answText = ansBtn.GetComponentInChildren<Text>();                                                          // Получаем дочерний текстовый объект нашей кнопки
+                answText.text = answersArray[0];                                                                                // Присваиваем текст к кнопке из answersArray[0], который был получен ранее (до тега "#->")
 			}
 		}
 	}
 
 	void Start(){
-		ReadFromFile(defaultDilogFile);
+		ReadFromFile(defaultDilogFile);    // Начинаем считывать файл заданный по умолчанию
 	}
 
-	public void ClearAll(){
-		dialogWindowText.text = "";
-		GameObject[] buttonList = GameObject.FindGameObjectsWithTag("Finish");
+	public void ClearAll(){                // Очистка ответов
+		dialogWindowText.text = string.Empty;
+		GameObject[] buttonList = GameObject.FindGameObjectsWithTag("Answer");
 		foreach(GameObject element in buttonList)
 		{
 			Destroy(element);
--- Assets/Scripts/DialogSystem/nextDilog.cs
+++ Assets/Scripts/DialogSystem/nextDilog.cs
@@ -4,13 +4,13 @@ using System.Collections;
 
 public class nextDilog : MonoBehaviour, IPointerDownHandler{
 
-	public string file;
+	public string file;         // Имя следующего файла с диалогом
 	
-	public void OnPointerDown(PointerEventData data)
+	public void OnPointerDown(PointerEventData data)                                                    // Принажатии на ответ..
 	{
-		GameObject.FindGameObjectWithTag("Canvas").GetComponent<FileReader>().ClearAll();
-		if(file != null && file != "")
-			GameObject.FindGameObjectWithTag("Canvas").GetComponent<FileReader>().ReadFromFile(file);
+		GameObject.FindGameObjectWithTag("Canvas").GetComponent<FileReader>().ClearAll();               // Удаляем все кнопки с ответами
+		if(file != string.Empty)
+            GameObject.FindGameObjectWithTag("Canvas").GetComponent<FileReader>().ReadFromFile(file);   // Считаваем новый файл с диалогом, имя которого попадает в переменную file сразу после нажатия кнопки с ответом (имя файла идет после тега "#->")
 
 	}
 }
